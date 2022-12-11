280d8106aab6acde639e80d46b03f58bc6a734bc
Sarvesh <sarveshgond0000@gmail.com>
2022-12-12 00:33:46 +0530

    ptr2

---
+++ ptr2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[10]={2,4,6};
+    
+    cout<<"the value at index first: "<<*arr<<endl;
+    cout<<"the value at index first: "<<*arr+1<<endl;
+}
